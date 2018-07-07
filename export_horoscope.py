d4e851146305f84ea69e39f0c8dcadbc94974fdc
sathyabhat <sathya@sathyasays.com>
2018-07-07 14:17:57 +0530

    create tables, connect to db when creating connection

--- export_horoscope.py
+++ export_horoscope.py
@@ -3,17 +3,17 @@
 from __future__ import unicode_literals
 from openpyxl import load_workbook
 from sys import exit
-import pymysql
+from pprint import pprint
 
+import pymysql
 import argparse
 
-def create_tables(connection, sheet):
-    pass
 
 def create_db_connection(args):
     connection = pymysql.connect(host=args.host,
                                  user=args.user,
-                                 passwd=args.password)
+                                 passwd=args.password,
+                                 db=args.database)
     return connection
 
 def create_database(connection):
@@ -27,13 +27,42 @@ def create_database(connection):
             print "{} {}".format(code, msg)
             exit(-1)
 
+def create_tables(connection, sheet, headers):
+    try:
+        pprint(sheet) 
+        pprint(headers)
+        sql_query = 'CREATE TABLE `{0}` ( \
+                                    `{1}` bigint(11) unsigned NOT NULL AUTO_INCREMENT, \
+                                    `{2}` varchar(11) CHARACTER SET utf8 DEFAULT NULL, \
+                                    `{3}` mediumtext CHARACTER SET utf8,  \
+                                    `{4}` text CHARACTER SET utf8,  \
+                                    `{5}` int(11) DEFAULT NULL,  \
+                                    `{6}` varchar(48) CHARACTER SET utf8 DEFAULT NULL, \
+                                    `{7}` mediumtext CHARACTER SET ascii,  \
+                                    PRIMARY KEY (`{1}`) \
+                                    ) ENGINE=InnoDB'.format(sheet,
+                                                            headers[0],
+                                                            headers[1],
+                                                            headers[2],
+                                                            headers[3],
+                                                            headers[4],
+                                                            headers[5],
+                                                            headers[6])
+        pprint('SQL Query: {}'.format(sql_query))
+        try:
+            connection.cursor().execute(sql_query)
+        except pymysql.err.InternalError as e:
+            code, msg = e.args
+            if code == 1050:
+                print "Table exists, not creating"
+            else
+                print "{} {}".format(code, msg)
+            exit(-1)
+
 def export_to_mysql(file_name, connection):
     book = load_workbook(file_name)
     sheets = book.sheetnames
     values = []
-    
-    for sheet in sheets:
-        create_tables(connection, sheet)
 
     for sheet in sheets:
         current_sheet = book.get_sheet_by_name(sheet)
@@ -42,9 +71,10 @@ def export_to_mysql(file_name, connection):
         
         for header in cells[0]:
             headers.append(header.value)
+        create_tables(connection, sheet, headers)
         for data in cells[1:]:
             values.append(data[0].value  + ' ' +  data[1].value.strftime('%Y-%m-%d') + ' ' + data[2].value + ' ' + data[3].value + ' ' + data[4].value + ' ' + data[5].value + ' ' + data[6].value)
-
+            
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser(description='Script to parse the horoscope Excel.')
@@ -52,7 +82,8 @@ if __name__=='__main__':
     parser.add_argument('--host', '-o', required=True, help='MySQL Host')
     parser.add_argument('--user', '-u', required=True, help='MySQL Username')
     parser.add_argument('--password', '-p', required=True, help='MySQL password')
+    parser.add_argument('--database', '-d', required=True, help='MySQL database to connect to')
     args = parser.parse_args()
     connection = create_db_connection(args)
-    create_database(connection)
+    #create_database(connection)
     export_to_mysql(args.file, connection)
